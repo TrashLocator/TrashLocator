7283075d211f05a7d0152395f4a32b0a19ba5567
takchiwan <ray325789@yahoo.com>
2014-03-27 21:35:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+    
+@app.route("/input")
+def input(): 
+    return render_template("input.html")
+    
 if __name__=="__main__":
     app.debug=True
     app.run(host='0.0.0.0',port=5000)
