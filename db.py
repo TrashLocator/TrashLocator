2da69934eab02590750408bdfb76a967c119e55c
Derek Tang <derek.tang.27@gmail.com>
2014-04-09 22:14:46 -0400

    updated log and getLatLng functions in db.py

--- db.py
+++ db.py
@@ -1,7 +1,25 @@
-connection = MongoClient()
-db = connection.database
+import pymongo
+from bson.objectid import ObjectId
 
-def loginfo(loc, lev):
-        db.login.insert({'location':loc, 'level':lev})
-        return True
-    
+import app
+import config as conf
+
+
+client = pymongo.MongoClient(conf.db)
+db = client.TPIS
+
+history = db.history
+
+def log(date, email, address, lat, lng):
+	history.insert({ "date" : date, "email": email, "address" : address, "lat" : lat, "lng" : lng})
+
+#logging 311 api
+#log(c['created_date'],'blah',c['incident_address'] + " " + c['incident_zip'],c['latitude'],c['longitude'])
+
+def getLatLng():
+    temp = history.find()
+    return temp
+
+#preparing coordinates for google maps
+#for x in a:
+#    temp = temp + "new google.maps.LatLng("+a[b]['latitude']+","+a[b]['longitude']+"), "
